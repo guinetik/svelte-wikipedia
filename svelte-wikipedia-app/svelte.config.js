3db4fcd9ecdd48a7846ec63aa9f7a33908211af5
guinetik <guinetik@gmail.com>
2022-06-15 14:44:50 -0300

    trying out svelte-kit build on githubpages

--- svelte-wikipedia-app/svelte.config.js
+++ svelte-wikipedia-app/svelte.config.js
@@ -1,15 +1,30 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from "svelte-preprocess"
+import preprocess from 'svelte-preprocess';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
 		preprocess({
-			postcss: true,
-		}),
+			postcss: true
+		})
 	],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		paths: {
+			base: dev ? '' : '/vanguard-js/svelte-wikipedia-app',
+		},
+		appDir: 'demo',
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
