cc5d539c663aad3784c05a7e2272f530305581ae
guinetik <guinetik@gmail.com>
2025-03-31 14:47:49 -0300

    Updates

---
+++ app/immutable/pages/about.svelte-9654c8f3.js
@@ -0,0 +1,31 @@
+import{S as ft,i as pt,s as bt,k as Z,e as n,t as r,_ as vt,d as i,m as tt,c as l,a as h,h as s,b as o,g as dt,I as t,j as mt,W as _t,a3 as xt,N as kt,a0 as yt}from"../chunks/index-f0dd9566.js";import{f as ht,Y as gt,a as ut}from"../chunks/index-1ae2aa9e.js";import"../chunks/singletons-e01e0108.js";function wt(E){let p,d,f,u,k,y=E[0]("title")+"",S,B,e,F,b,P,T,W,$,v,j,q,z,G,N,D,m,H,L,_,M,O,V,Y,x,J,K,Q,U,g,X,C,w,I;return{c(){p=Z(),d=n("section"),f=n("article"),u=n("div"),k=n("h1"),S=r(y),B=Z(),e=n("p"),F=r("A small app I build to study the "),b=n("a"),P=r("Svelte"),T=r(`\r
+				frontend framework.\r
+				`),W=n("br"),$=r(`\r
+				It uses\r
+				`),v=n("a"),j=r("Wikimedia API"),q=r(`\r
+				to fetch searched pages.\r
+				`),z=n("br"),G=r(`\r
+				Also the most visited pages are displayed once the app loads.\r
+				`),N=n("br"),D=r(`\r
+				For styling I used\r
+				`),m=n("a"),H=r("TailwindCSS"),L=r(`\r
+				with some pre-build components by\r
+				`),_=n("a"),M=r("Flowbite"),O=r(`.\r
+				`),V=n("br"),Y=r(`\r
+				Check out the code on `),x=n("a"),J=r("Github"),K=r(`!\r
+				`),Q=n("br"),U=Z(),g=n("i"),X=r("I know the app has internationalization but I'm not translating this about into 5 languages lmao"),this.h()},l(c){vt('[data-svelte="svelte-1ine71f"]',document.head).forEach(i),p=tt(c),d=l(c,"SECTION",{class:!0});var et=h(d);f=l(et,"ARTICLE",{class:!0});var at=h(f);u=l(at,"DIV",{class:!0});var R=h(u);k=l(R,"H1",{class:!0});var rt=h(k);S=s(rt,y),rt.forEach(i),B=tt(R),e=l(R,"P",{class:!0});var a=h(e);F=s(a,"A small app I build to study the "),b=l(a,"A",{class:!0,href:!0});var st=h(b);P=s(st,"Svelte"),st.forEach(i),T=s(a,`\r
+				frontend framework.\r
+				`),W=l(a,"BR",{}),$=s(a,`\r
+				It uses\r
+				`),v=l(a,"A",{class:!0,href:!0});var nt=h(v);j=s(nt,"Wikimedia API"),nt.forEach(i),q=s(a,`\r
+				to fetch searched pages.\r
+				`),z=l(a,"BR",{}),G=s(a,`\r
+				Also the most visited pages are displayed once the app loads.\r
+				`),N=l(a,"BR",{}),D=s(a,`\r
+				For styling I used\r
+				`),m=l(a,"A",{class:!0,href:!0});var lt=h(m);H=s(lt,"TailwindCSS"),lt.forEach(i),L=s(a,`\r
+				with some pre-build components by\r
+				`),_=l(a,"A",{class:!0,href:!0});var ot=h(_);M=s(ot,"Flowbite"),ot.forEach(i),O=s(a,`.\r
+				`),V=l(a,"BR",{}),Y=s(a,`\r
+				Check out the code on `),x=l(a,"A",{class:!0,href:!0});var it=h(x);J=s(it,"Github"),it.forEach(i),K=s(a,`!\r
+				`),Q=l(a,"BR",{}),U=tt(a),g=l(a,"I",{});var ct=h(g);X=s(ct,"I know the app has internationalization but I'm not translating this about into 5 languages lmao"),ct.forEach(i),a.forEach(i),R.forEach(i),at.forEach(i),et.forEach(i),this.h()},h(){document.title="About",o(k,"class","dark:text-white text-gray-800 text-6xl"),o(b,"class","text-blue-500 dark:text-cyan-300 hover:text-blue-700 dark:hover:text-cyan-500 hover:underline"),o(b,"href","https://svelte.dev/"),o(v,"class","text-blue-500 dark:text-cyan-300 hover:text-blue-700 dark:hover:text-cyan-500 hover:underline"),o(v,"href","https://www.mediawiki.org/wiki/API:Main_page/pt-br"),o(m,"class","text-blue-500 dark:text-cyan-300 hover:text-blue-700 dark:hover:text-cyan-500 hover:underline"),o(m,"href","https://tailwindcss.com/"),o(_,"class","text-blue-500 dark:text-cyan-300 hover:text-blue-700 dark:hover:text-cyan-500 hover:underline"),o(_,"href","https://flowbite.com/"),o(x,"class","text-blue-500 dark:text-cyan-300 hover:text-blue-700 dark:hover:text-cyan-500 hover:underline"),o(x,"href","https://github.com/guinetik/svelte-wikipedia/"),o(e,"class","mt-3 dark:text-gray-300 leading-10"),o(u,"class","mt-2"),o(f,"class","flex justify-center"),o(d,"class","grid")},m(c,A){dt(c,p,A),dt(c,d,A),t(d,f),t(f,u),t(u,k),t(k,S),t(u,B),t(u,e),t(e,F),t(e,b),t(b,P),t(e,T),t(e,W),t(e,$),t(e,v),t(v,j),t(e,q),t(e,z),t(e,G),t(e,N),t(e,D),t(e,m),t(m,H),t(e,L),t(e,_),t(_,M),t(e,O),t(e,V),t(e,Y),t(e,x),t(x,J),t(e,K),t(e,Q),t(e,U),t(e,g),t(g,X),I=!0},p(c,[A]){(!I||A&1)&&y!==(y=c[0]("title")+"")&&mt(S,y)},i(c){I||(_t(()=>{w&&w.end(1),C=yt(d,ht(ut),{}),C.start()}),I=!0)},o(c){C&&C.invalidate(),w=xt(d,ht(ut),{}),I=!1},d(c){c&&i(p),c&&i(d),c&&w&&w.end()}}}function It(E,p,d){let f;return kt(E,gt,u=>d(0,f=u)),[f]}class Ct extends ft{constructor(p){super(),pt(this,p,It,wt,bt,{})}}export{Ct as default};
